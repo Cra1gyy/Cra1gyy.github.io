7d5d04416ce5ab6249d1c61676f2e7e8d8c50fc0
craigy <128209435+Cra1gyy@users.noreply.github.com>
2024-03-22 14:13:01 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const rotating = document.getElementById('image_rotate');
 const rotate_left_button = document.getElementById('rotate_left');
 const rotate_right_button = document.getElementById('rotate_right');
+const center_button = document.getElementById('center');
+
+
 
 let rotate_func_left = () => {
     rotating.style.transform = 'rotate(-20deg)'
@@ -8,8 +11,18 @@ let rotate_func_left = () => {
 
 rotate_left_button.addEventListener('click', rotate_func_left);
 
+
+
 let rotate_func_right = () => {
     rotating.style.transform = 'rotate(20deg)' 
 }
 
 rotate_right_button.addEventListener('click', rotate_func_right);
+
+
+
+let center_func = () => {
+    rotating.style.transform = 'rotate(0deg)'
+}
+
+center_button.addEventListener('click', center_func);
